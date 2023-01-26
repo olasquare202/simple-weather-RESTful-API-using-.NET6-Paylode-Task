7613f7eb02d30ac072446b94f91848f089253252
chrisshola1 <tkwalks1@gmail.com>
2023-01-26 11:50:02 +0100

    A simple weather RESTful API using .Net6 Paylode task

---
+++ WebApplication1/Model/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Model
+{
+    public class LoginModel
+    {
+
+
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+
+        //[Required(ErrorMessage = "User Name is required")]
+        //public string? Username { get; set; }
+
+        //[Required(ErrorMessage = "Password is required")]
+        //public string? Password { get; set; }
+    }
+}
---
+++ WebApplication1/Model/RegisterModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Model
+{
+    public class RegisterModel
+    {
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+
+
+
+
+
+        //[Required(ErrorMessage = "User Name is required")]
+        //public string? Username { get; set; }
+
+        //[EmailAddress]
+        //[Required(ErrorMessage = "Email is required")]
+        //public string? Email { get; set; }
+
+        //[Required(ErrorMessage = "Password is required")]
+        //public string? Password { get; set; }
+    }
+}
